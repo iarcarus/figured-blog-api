d5f78c22d4b789639b4eabec2fd1bc3966b31aff
Medalha <andre.medalhaa@gmail.com>
2019-08-26 03:01:28 -0300

    [NEW] - Adding exception messages

---
+++ resources/lang/en/exception.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'general' => [
+        'service_invalid' => 'Service not declared.'
+    ],
+    'user'    => [
+        'not_found'            => 'User not found.',
+        'cannot_delete_itself' => 'User cannot delete itself.'
+    ],
+    'post'    => [
+        'not_found' => 'Post not found.'
+    ],
+];
